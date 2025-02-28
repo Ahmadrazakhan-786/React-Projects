f46a94da9ea6b3f33d6bc522eb1fb8d58ec72f8e
Ahmadraza <razakhan.srt786@gmail.com>
2025-02-28 23:32:45 +0530

    Task list

--- ems/src/Components/Dashboard/EmployeeDashboard.jsx
+++ ems/src/Components/Dashboard/EmployeeDashboard.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Header from '../Others/Header'
+import TaskListNumbers from '../Others/TaskListNumbers'
+import TaskList from '../TaskList/TaskList'
 
 function EmployeeDashboard() {
   return (
-    <div>
-        <div>
-            
-        </div>
+    <div className='p-10 bg-[#1C1C1C] h-screen'>
+      <Header />
+      <TaskListNumbers />
+      <TaskList />
     </div>
   )
 }
--- ems/src/Components/Others/Header.jsx
+++ ems/src/Components/Others/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+
 function Header() {
   return (
-    <div>
-        <h1>Hello <br /> Ahmad</h1>
+    <div className='flex items-end justify-between'>
+        <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>Ahmad 🖐️</span></h1>
+        <button className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-sm'>Log Out</button>
     </div>
   )
 }
