545cd120b0fd45dd60d1ec715c58ccf7ce5e426c
Ahmadraza <razakhan.srt786@gmail.com>
2025-03-01 23:51:11 +0530

    Tasklist

--- ems/src/Components/TaskList/TaskList.jsx
+++ ems/src/Components/TaskList/TaskList.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 function TaskList() {
   return (
-    <div className='h-[55%] w-full bg-red-500 mt-10'>
-        
+    <div id='tasklist' className='h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5  mt-10'>
+        <div className='flex-shrink-0 h-full w-[300px] bg-red-400 rounded-xl'>
+            <div>
+              <h3 className='bg-red-600'>High</h3>
+              <h4>28 feb 2025</h4>
+            </div>
+        </div>
     </div>
   )
 }
