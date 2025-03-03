c91b1a3ede1939ee1e36ff9658cc67f6ba214b42
Ahmadraza <razakhan.srt786@gmail.com>
2025-03-03 05:42:54 +0530

    Admin Dashboard

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Login from './Components/Auth/Login'
 import EmployeeDashboard from './Components/Dashboard/EmployeeDashboard'
+import AdminDashboard from './Components/Dashboard/AdminDashboard'
 
 function App() {
   return (
     <>
 
     {/* <Login /> */}
-    <EmployeeDashboard />
+    {/* <EmployeeDashboard /> */}
+    <AdminDashboard />
     
     </>
   )
