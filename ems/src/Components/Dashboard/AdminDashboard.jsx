e3481bf3b6872550fbaac63c9d426149b8111b4f
Ahmadraza <razakhan.srt786@gmail.com>
2025-03-05 16:55:10 +0530

    Dashboard

--- ems/src/Components/Dashboard/AdminDashboard.jsx
+++ ems/src/Components/Dashboard/AdminDashboard.jsx
@@ -7,17 +7,21 @@ function AdminDashboard() {
         <Header />
 
         <div>
-            <form>
-                <h3>Task Title</h3>
-                <input type="text" placeholder='Make a UI design' />
-                <h3>Description</h3>
-                <textarea name="" id="" cols="30" rows="10"></textarea>
-                <h3>Date</h3>
-                <input type="date" />
-                <h3>Assign to</h3>
-                <input type="text" placeholder='Employee name' />
-                <h3>Category</h3>
-                <input type="text" placeholder='Design, Dev, etc.' />
+            <form className='flex flex-wrap w-full bg-red-200 items-start justify-between'>
+              <div className='w-1/2'>
+              <div><h3>Task Title</h3>
+              <input type="text" placeholder='Make a UI design' /></div>
+              <div><h3>Date</h3>
+                <input type="date" /></div>
+               <div> <h3>Assign to</h3>
+               <input type="text" placeholder='Employee name' /></div>
+                <div><h3>Category</h3>
+                <input type="text" placeholder='Design, Dev, etc.' /></div>
+              </div>
+                <div><h3>Task Title</h3>
+                <input type="text" placeholder='Make a UI design' /></div>
+                <div className=''><h3>Description</h3>
+                <textarea name="" id="" cols="30" rows="10"></textarea></div>
                 <button>Create Task</button>
             </form>
         </div>
