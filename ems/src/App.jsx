b467a477af4193666a1a495903dc82b7b90e83a6
Ahmadraza <razakhan.srt786@gmail.com>
2025-03-10 23:30:17 +0530

    landing page

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Login from './Components/Auth/Login'
 import EmployeeDashboard from './Components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './Components/Dashboard/AdminDashboard'
 import { getLocalStorage, setLocalStorage } from './Utils/localStorage'
+import { AuthContext } from './Context/AuthProvider'
 
 const App = () => {
 
@@ -12,19 +13,21 @@ const App = () => {
   const handleLogin = (email, password)=>{
     if(email == 'admin@ark.com' && password == '123'){
       setUser('admin')
-      console.log(user)
     }else if(email == 'user@ark.com' && password == '123'){
       setUser('Employee')
-      console.log(user)
     }
     else{
       alert('Invalid Credentials')
     }
   }
+
+  const data = useContext(AuthContext)
+  console.log(data)
     
    return (
     <>
     {!user ? <Login handleLogin={handleLogin} /> : ''}
+    {user == 'admin' ? <AdminDashboard/> : <EmployeeDashboard/>}
     {/* <EmployeeDashboard /> */}
     {/* <AdminDashboard /> */}
     
--- ems/src/Components/Auth/Login.jsx
+++ ems/src/Components/Auth/Login.jsx
@@ -46,3 +46,4 @@ const Login = ({handleLogin}) => {
 }
 
 export default Login
+
