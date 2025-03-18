752756bfb7629599afa0fc7b9b8c4720b1fcb606
Ahmadraza <razakhan.srt786@gmail.com>
2025-03-18 22:52:26 +0530

    form handling

--- ems/src/Components/Others/CreateTask.jsx
+++ ems/src/Components/Others/CreateTask.jsx
@@ -12,36 +12,69 @@ const CreateTask = () => {
 
     const submitHandler = (e) =>{
       e.preventDefault()
-      console.log("task created")
+
+      setTaskdate("")
+      setAssignTo("")
+      setCategory("")
+      setTaskDescription("")
+      setTaskTitle("")  
+      
     }
 
   return (
     <div className='p-5 bg-[#1c1c1c] mt-7 rounded'>
             <form onSubmit={(e)=>{
               submitHandler(e)
-            }} className='flex flex-wrap w-full items-start justify-between'>
+            }} 
+            className='flex flex-wrap w-full items-start justify-between'
+            >
               <div className='w-1/2'>
               <div>
                 <h3 className='text-sm text-gray-300 mb-0.5'>Task Title</h3>
-              <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Make a UI design' />
+              <input
+              value={taskTitle}
+              onChange={(e)=>{
+                setTaskTitle(e.target.value)
+              }}
+               className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Make a UI design' />
               </div>
               <div>
                 <h3 className='text-sm text-gray-300 mb-0.5'>Date</h3>
-                <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="date" />
+                <input
+                  value={taskdate}
+                  onChange={(e)=>{
+                    setTaskdate(e.target.value)
+                  }}
+                 className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="date" />
                 </div>
                <div> 
                 <h3 className='text-sm text-gray-300 mb-0.5'>Assign to</h3>
-               <input  className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Employee name' />
+               <input 
+               value={assignTo}
+               onChange={(e)=>{
+                 setAssignTo(e.target.value)
+               }}
+                className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Employee name' />
                </div>
                 <div>
                   <h3 className='text-sm text-gray-300 mb-0.5'>Category</h3>
-                <input  className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Design, Dev, etc.' />
+                <input  
+                value={category}
+                onChange={(e)=>{
+                  setCategory(e.target.value)
+                }}
+                className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Design, Dev, etc.' />
                 </div>
               </div>
               
                 <div>
                 <h3 className='text-sm text-gray-300 mb-0.5'>Description</h3>
-                <textarea className='w-full h-44 text-sm py-2 px-10 rounded outline-none bg-transparent border-[1px] border-gray-400 ' name="" id="" ></textarea>
+                <textarea 
+                value={taskDescription}
+                onChange={(e)=>{
+                  setTaskDescription(e.target.value)
+                }}
+                className='w-full h-44 text-sm py-2 px-10 rounded outline-none bg-transparent border-[1px] border-gray-400 ' name="" id="" ></textarea>
                 <button className='bg-emerald-500 py-3 hover:bg-emerald-600 px-5 rounded text-sm mt-4 w-full'>Create Task</button>
                 </div>
                 
