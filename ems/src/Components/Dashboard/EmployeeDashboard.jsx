11060fb2bfeb5636a1fc345f473009d7374ef607
Ahmadraza <razakhan.srt786@gmail.com>
2025-04-02 07:12:07 +0530

    final commit

--- ems/src/Components/Dashboard/EmployeeDashboard.jsx
+++ ems/src/Components/Dashboard/EmployeeDashboard.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
+import { useContext,useEffect } from 'react'
 import Header from '../Others/Header'
 import TaskListNumbers from '../Others/TaskListNumbers'
 import TaskList from '../TaskList/TaskList'
+import { AuthContext } from '../../Context/AuthProvider'
 
-const EmployeeDashboard = (props) => {
-  // localStorage.clear()
+const EmployeeDashboard = ({ data, changeUser }) => {
+  const [userData, setUserData] = useContext(AuthContext)
+
+  useEffect(() => {
+    if (userData) {
+      const updatedEmployee = userData.find(emp => emp.id === data.id)
+      if (updatedEmployee) {
+        const updatedData = { role: "employee", data: updatedEmployee }
+        localStorage.setItem('loggedInUser', JSON.stringify(updatedData))
+      }
+    }
+  }, [userData, data.id])
+
+  const currentEmployeeData = userData?.find(emp => emp.id === data.id) || data
+
   return (
-    <div className='p-10 bg-[#1C1C1C] h-screen'>
-
-      <Header changeUser={props.changeUser} data={props.data}/>
-      <TaskListNumbers data={props.data}/>
-      <TaskList data={props.data}/>
-
+    <div className='min-h-screen bg-[#0a0a0a] p-6 md:p-10'>
+      <Header data={currentEmployeeData} changeUser={changeUser} />
+      <TaskListNumbers data={currentEmployeeData} />
+      <TaskList data={currentEmployeeData} />
     </div>
   )
 }
